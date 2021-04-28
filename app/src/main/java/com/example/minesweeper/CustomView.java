3eaec427f74574c98ca9b7aa525dfae5d553a952
Lina Diringer <linadiringer@gmail.com>
2021-04-28 11:21:58 +0100

    milestone 4: placement of the 20 mines

--- app/src/main/java/com/example/minesweeper/CustomView.java
+++ app/src/main/java/com/example/minesweeper/CustomView.java
@@ -1,30 +1,26 @@
 package com.example.minesweeper;
 
 import android.content.Context;
-import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
-import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
-import android.util.AttributeSet;
-import android.view.View;
-
-import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CustomView extends View {
 
     private TextPaint mTextPaint;
-    private Paint rectPaint;
+    private Paint black;
+    private Paint gray;
+    private Paint red;
+
     ArrayList<Cell> cells = new ArrayList<>(100);
 
     Rect square;
@@ -48,6 +44,18 @@ public class CustomView extends View {
         setBackgroundColor(Color.WHITE);
         for (int i = 0; i < 100; i++)
             cells.add(new Cell());
+        int count = 0;
+        while (count < 20)
+        {
+            Random rand = new Random();
+            int x = rand.nextInt(10);
+            int y = rand.nextInt(10);
+            if (!cells.get(y * 10 + x).isMined())
+            {
+                cells.get(y * 10 + x).setMined(true);
+                count++;
+            }
+        }
     }
 
     @Override
@@ -59,31 +67,43 @@ public class CustomView extends View {
 
         int contentWidth = getWidth() - paddingLeft - paddingRight;
 
-        int rectBounds = contentWidth / 10;
+        int squareSize = contentWidth / 10;
 
-        int sideLength = rectBounds - 10;
+        int sideLength = squareSize - 10;
 
-        rectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        rectPaint.setColor(Color.BLACK);
+        // setting colors
+        black = new Paint();
+        black.setColor(Color.BLACK);
+        gray = new Paint();
+        gray.setColor(Color.GRAY);
+        red = new Paint();
+        red.setColor(Color.RED);
 
         square = new Rect(10, 10, sideLength, sideLength);
+
 
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 canvas.save();
-                canvas.translate(j * rectBounds, i * rectBounds);
-                if (!cells.get(i * 10 + j).isCovered())
-                    rectPaint.setColor(Color.GRAY);
+                canvas.translate(j * squareSize, i * squareSize);
+                if (cells.get(i * 10 + j).isCovered())
+                {
+                    canvas.drawRect(square, black);
+                }
                 else {
                     if (cells.get(i * 10 + j).isMined())
-                        rectPaint.setColor(Color.RED);
+                    {
+                        canvas.drawRect(square, red);
+                        black.setTextSize(80);
+                        canvas.drawText("M", 20, 80, black);
+                    }
                     else
-                        rectPaint.setColor(Color.BLACK);
+                        canvas.drawRect(square, gray);
                 }
-                canvas.drawRect(square, rectPaint);
                 canvas.restore();
             }
         }
+
     }
 
     public boolean onTouchEvent(MotionEvent event) {
@@ -100,14 +120,10 @@ public class CustomView extends View {
         int row = y / caseSize;
         int col = x / caseSize;
 
-        if (eventAction == MotionEvent.ACTION_DOWN)
+        if (cells.get(10 * row + col).isCovered() && eventAction == MotionEvent.ACTION_DOWN)
             cells.get(10 * row + col).setCovered(false);
 
-        // tell the View to redraw the Canvas
         invalidate();
-
-        // tell the View that we handled the event
         return true;
-
     }
 }
